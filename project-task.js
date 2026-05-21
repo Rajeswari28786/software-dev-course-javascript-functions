a4bf9fd5b5dfa864815be808d52bc28f00a83c1d
Raji <raji.psna@gmail.com>
2026-05-21 11:40:30 -0500

    Function Refactoring Activity

--- project-task.js
+++ project-task.js
@@ -71,4 +71,39 @@ for (let i = 0; i < names.length; i++) {
    - Keep your code clean, readable, and easy to maintain
 */
 
+console.log("--------------------------");
+
 // ✅ Your refactored code goes below this line!
+// Script 1 - Greeting multiple users
+const greetUser = function(name)
+{
+   console.log("Welcome, "+name+"!");
+};
+greetUser("Alice");
+greetUser("Bob");
+greetUser("Charlie");
+
+// Script 2 - Sum calculation
+const addNumbers = function(num1,num2)
+{
+   return num1 + num2;
+};
+console.log( "The sum of 5 and 10 is " +addNumbers(5,10));
+
+// Script 3 - Product calculation
+const productNumbers = function(num1,num2)
+{
+   return num1 * num2;
+};
+console.log("The product of 5 and 10 is " +productNumbers(5,10));
+
+// Script 4 - Print names from a list
+console.log("Names in the list:");
+function printNames(names)
+{
+for (let i = 0; i < names.length; i++)
+{
+ console.log(names[i]);
+}
+}
+printNames(["Alice", "Bob", "Charlie"]);
